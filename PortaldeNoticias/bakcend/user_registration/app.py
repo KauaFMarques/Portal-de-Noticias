2afeddedad9030269efe3d1653de227ec070f96b
JVitor21 <jvlimasantos0@gmail.com>
2024-04-09 15:53:13 -0300

    adicionando chave e blueprint

--- PortaldeNoticias/bakcend/user_registration/app.py
+++ PortaldeNoticias/bakcend/user_registration/app.py
@@ -3,6 +3,9 @@ from flask import Flask
 from user import user_bp
 
 app = Flask(__name__)
+# Define uma chave secreta para a aplicação Flask.
+app.secret_key = '2002'
+# Registra o blueprint user_bp na aplicação Flask, atribuindo-lhe o prefixo de URL '/user'.
 app.register_blueprint(user_bp, url_prefix='/user')
 
 if __name__ == '__main__':
