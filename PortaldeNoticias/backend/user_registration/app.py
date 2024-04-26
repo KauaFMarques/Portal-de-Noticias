4d2f9720ca803f43b2d9b859a648834469c8bf99
JVitor21 <jvlimasantos0@gmail.com>
2024-04-26 14:49:08 -0300

    Alteração, add cors para web

--- PortaldeNoticias/backend/user_registration/app.py
+++ PortaldeNoticias/backend/user_registration/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from noticias import noticias_bp
 from user import user_bp
 
 app = Flask(__name__)
+cors = CORS(app)
 # Define uma chave secreta para a aplicação Flask.
 app.secret_key = '2002'
 # Registra o blueprint user_bp na aplicação Flask, atribuindo-lhe o prefixo de URL '/user'.
