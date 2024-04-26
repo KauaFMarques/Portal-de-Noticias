4687ba314c388fabbfff7dbac5d6802d16547e43
EngelsAS <engelsantero@gmail.com>
2024-04-26 19:31:00 -0300

    Finalizacao NavbarLateral

--- frontend/src/components/Navbar/NavbarLateral.jsx
+++ frontend/src/components/Navbar/NavbarLateral.jsx
@@ -1,5 +1,7 @@
 import styles from "./NavbarLateral.module.css";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { Link } from "react-router-dom";
+import { categorys } from "../../utils/categorys";
 
 const NavbarLateral = ({ setMenuLateralAberto, menuLateralAberto }) => {
   return (
@@ -7,6 +9,22 @@ const NavbarLateral = ({ setMenuLateralAberto, menuLateralAberto }) => {
       className={styles.container_nav_lateral}
       style={menuLateralAberto ? null : { marginLeft: "-1000px" }}
     >
+      <div className={styles.header_nav_lateral}>
+        <Link to={"/login"} style={{ marginBottom: "10px" }}>
+          Fazer Login
+        </Link>
+        <Link to={"/register"}>Cadastre-se</Link>
+      </div>
+      <div className={styles.nav_lateral_categorias}>
+        <h3>Categorias</h3>
+        <ul>
+          {categorys.map((category, index) => (
+            <li key={index}>
+              <Link to="/">{category}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
       <div
         className={styles.arrow_back}
         onClick={() => setMenuLateralAberto(false)}
