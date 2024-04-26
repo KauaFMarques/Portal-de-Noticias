e356ae6018fb58fcc67ce3d50bb2953cc476fc1c
JVitor21 <jvlimasantos0@gmail.com>
2024-04-26 18:43:44 -0300

    Removendo o prefixo de URL de app.py

--- PortaldeNoticias/backend/user_registration/app.py
+++ PortaldeNoticias/backend/user_registration/app.py
@@ -8,7 +8,7 @@ cors = CORS(app)
 # Define uma chave secreta para a aplicação Flask.
 app.secret_key = '2002'
 # Registra o blueprint user_bp na aplicação Flask, atribuindo-lhe o prefixo de URL '/user'.
-app.register_blueprint(user_bp, url_prefix='/user')
-app.register_blueprint(noticias_bp, url_prefix='/notica')
+app.register_blueprint(user_bp)
+app.register_blueprint(noticias_bp)
 if __name__ == '__main__':
     app.run(debug=True)
