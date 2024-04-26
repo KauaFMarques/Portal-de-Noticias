13d71c39c5a53e4055b6b02f9fe0212a5865c337
KauaFMarques <kauamarques52@outlook.com>
2024-04-26 17:10:15 -0300

    alteração do login de joão vitor

--- backend/user_registration/user.py
+++ backend/user_registration/user.py
@@ -104,10 +104,16 @@ def login():
         if user:
             user_data = {
                 'username': user[1],
-                'email': user[2]
+                'email': user[2],
+                'id': user[0]
             }
             session['logged_in'] = True
-            return jsonify({'success': True, 'message': 'Login efetuado com sucesso!'})
+            respose_data = {
+                'success' : True,
+                'message' : 'Login efetuado com sucesso!',
+                'user' : user_data
+            }
+            return jsonify(respose_data)
         else:
             cursor.execute("SELECT * FROM users WHERE username=%", (username,))
             existing_user = cursor.fetchone()
